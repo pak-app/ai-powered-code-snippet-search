849993d310d1caba3aac7707ec0494a6eb580bf4
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-07-28 01:09:51 +0330

    define model document of database Schematic

---
+++ client-api/src/models/snippet.ts
@@ -0,0 +1,47 @@
+// models/Snippet.ts
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ISnippet extends Document {
+    code: string;
+    language: string;
+    description: string;
+    tags: string[];
+    embedding: number[],
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const SnippetSchema: Schema = new Schema<ISnippet>(
+    {
+        code: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        language: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        tags: {
+            type: [String],
+            default: []
+        },
+        embedding: {
+            type: [Number],
+            required: true,
+            validate: {
+                validator: (arr: number[]) => arr.every((val) => typeof val === "number"),
+                message: "Embedding must be an array of numbers (float or int)."
+            }
+        }
+    },
+    { timestamps: true } // adds createdAt and updatedAt
+);
+
+export default mongoose.model<ISnippet>("Snippet", SnippetSchema);
