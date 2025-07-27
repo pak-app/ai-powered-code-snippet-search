f4ab5f58c3e7666befce361f48d98e29cc289fd6
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-07-28 01:09:26 +0330

    define controller for snippet route

---
+++ client-api/src/controller/snippet.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+// import { SnippetPOSTBody } from '../types/createSnippet';
+
+export function createSnippet(
+    req: Request,
+    res: Response
+) {
+
+}
+
+export function searchSnippet(
+    req: Request,
+    res: Response
+){
+
+}
