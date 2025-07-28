33d8dc9fe9204c7817ae742c038aa6f4ef4e8b8a
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-07-28 19:57:54 +0330

    add __init__.py file to resolve addressing

--- embedding-api/gRPCMethods/embedding_pb2_grpc.py
+++ embedding-api/gRPCMethods/embedding_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import embedding_pb2 as embedding__pb2
+from . import embedding_pb2 as embedding__pb2
 
 GRPC_GENERATED_VERSION = '1.74.0'
 GRPC_VERSION = grpc.__version__
