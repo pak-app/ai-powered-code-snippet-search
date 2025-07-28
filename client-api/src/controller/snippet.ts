045b6588379c9fdfef377fd280c292b02a8451e5
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-07-28 21:54:23 +0330

    complete POST method

--- client-api/src/controller/snippet.ts
+++ client-api/src/controller/snippet.ts
@@ -1,11 +1,27 @@
 import { Request, Response } from 'express';
-// import { SnippetPOSTBody } from '../types/createSnippet';
+import { generateEmbeddingAsync } from '../utils/gRPCClient';
+import { SnippetPOSTBody } from '../types/createSnippet';
+import { AppError } from '../utils/errors';
+import { EmbeddingResponse } from '../types/gRPCClient';
 
-export function createSnippet(
+export async function createSnippet(
     req: Request,
     res: Response
 ) {
+    const body: SnippetPOSTBody = req.body;
 
+    const deadline = new Date(Date.now() + 10000);
+    const gRPCResponse: EmbeddingResponse = await generateEmbeddingAsync(
+        body,
+        { deadline }
+    );
+    
+    if(!gRPCResponse)
+        throw new AppError('Unexpected error');
+
+    res.status(200).json({
+        embed: gRPCResponse.embedding
+    });
 }
 
 export function searchSnippet(
