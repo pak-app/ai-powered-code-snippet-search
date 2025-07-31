219c7b0571c7b3fe08d74ad3e4f0d33e5336598e
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-07-31 10:59:29 +0330

    improve the joi schema

--- client-api/src/utils/joiBodies/createSnippet.ts
+++ client-api/src/utils/joiBodies/createSnippet.ts
@@ -1,11 +1,13 @@
 
 import Joi from 'joi';
 
-export const createSnippetSchema = Joi.object({
+const createSnippetSchema = Joi.object({
     code: Joi.string().min(2).required().trim(),
     describe: Joi.string().required().trim(),
     language: Joi.string().trim(),
     tags: Joi.array().items(
-        Joi.string().min(2).required()
+        Joi.string().min(2).trim()
     ).required().min(1)
 });
+
+export default createSnippetSchema;
