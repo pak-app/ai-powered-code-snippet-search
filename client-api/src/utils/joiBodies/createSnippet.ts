06de1ad1616af77d3a6205ac06d458948ef66b89
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-07-31 11:12:05 +0330

    fix the key of Joi schema

--- client-api/src/utils/joiBodies/createSnippet.ts
+++ client-api/src/utils/joiBodies/createSnippet.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 
 const createSnippetSchema = Joi.object({
     code: Joi.string().min(2).required().trim(),
-    describe: Joi.string().required().trim(),
+    description: Joi.string().required().trim(),
     language: Joi.string().trim(),
     tags: Joi.array().items(
         Joi.string().min(2).trim()
