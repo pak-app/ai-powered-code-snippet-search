fc0c1877d840aa0ce626914b7f41a47992069070
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-07-31 10:59:46 +0330

    check body middleware of POST request

--- client-api/src/routes/snippet.ts
+++ client-api/src/routes/snippet.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as controller from '../controller/snippet';
+import validateBody from '../middlewares/validator';
+import createSnippetSchema from '../utils/joiBodies/createSnippet';
 
 const router = Router();
 
-router.post('/create', controller.createSnippet);
+router.post('/create', validateBody(createSnippetSchema, 'body'), controller.createSnippet);
 // router.get('/snippet/search');
 
 export default router;
