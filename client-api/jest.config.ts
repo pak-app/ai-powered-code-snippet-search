ff4710d96dd3d2b9e5de00812d6e67432d178d54
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-08-02 09:47:30 +0330

    define jest.config.ts file for jest

---
+++ client-api/jest.config.ts
@@ -0,0 +1,19 @@
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.[jt]s', '**/?(*.)+(spec|test).[jt]s'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
+
+export default config;
